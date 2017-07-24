8a7f11900978d5ede26ff11ee57bb9176238bfa8
Sally Bouley <sallybouley87@gmail.com>
2017-07-24 17:07:20 -0600

    database

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const knex = require('./db/knex');
+//the app.use(cors()) needs to be above routes
+app.use(cors());
+
+app.get('/', (request,response) => {
+  knex('place').then((data) => {
+    response.json(data);
+  })
+})
 
 app.listen(8000);
