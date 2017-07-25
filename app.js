1b350ada2117281d71275bfdb136c7aa79bf8e48
Sally Bouley <sallybouley87@gmail.com>
2017-07-24 18:16:21 -0600

    object

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ const knex = require('./db/knex');
 //the app.use(cors()) needs to be above routes
 app.use(cors());
 
-// app.get('/', (request,response) => {
-//   knex('place').then((data) => {
-//     response.json(data);
-//   })
-// })
-
-app.get('/', (request,response) => {
-  response.send("Hello....");
+app.get('/places', (request,response) => {
+  knex('place').then((data) => {
+    response.json(data);
+  });
 });
+
 app.listen(process.env.PORT || 8000);
