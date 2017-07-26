f5ca55ca21513c89048e1c0369cda3759fcc8d9f
Sally Bouley <sallybouley87@gmail.com>
2017-07-25 21:58:20 -0600

    routes

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const knex = require('./db/knex');
+const bodyParser = require('body-parser')
 //the app.use(cors()) needs to be above routes
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/places', (request,response) => {
   knex('place').then((data) => {
@@ -17,6 +20,38 @@ app.get('/places/:id', (request,response) => {
   .where('id',id).first()
   .then((data) => {
     response.json(data)
+  });
+});
+
+app.post('/places', (request,response) => {
+  let post = request.body;
+  knex('place').insert(post)
+  .returning('*')
+  .then(data => {
+    response.json(data);
+  });
+});
+
+app.put('/places/:id', (request,response) => {
+  let id = request.params.id;
+  let edit = request.body;
+  knex('place').where('id',id)
+  .update(edit)
+  .returning('*')
+  .then(edited => {
+    response.json(edited);
+  });
+});
+
+app.delete('/places/:id', (request,response) => {
+  let id = request.params.id;
+  knex('place')
+  .where('id',id).del()
+  .then(deleted => {
+    response.json({
+      message: 'Location deleted',
+      deleted: deleted
+    });
   });
 });
 
