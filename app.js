348f507d159acfa864a30e2b6d91a078f0cbb9cd
Sally Bouley <sallybouley87@gmail.com>
2017-07-28 21:50:52 -0600

    invalids

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.get('/places', (request,response) => {
 });
 
 function validPlace(data) {
-  let validCity = typeof data.city == 'string' && data.city.trim() != '';
-  let validState = typeof data.state == 'string' && data.state.trim() != '';
+  let validCity = typeof data.city === 'string' && data.city.trim() != '';
+  let validState = typeof data.state === 'string' && data.state.trim() != '';
   let validRating = !isNaN(data.rating);
   return validCity && validState && validRating;
 }
